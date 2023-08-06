42e665c08cd1ea4db0b15d5a963c73d1ba9a7afc
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-08-06 13:41:30 +0100

    Replot calibration

--- test_calibration.py
+++ test_calibration.py
@@ -9,14 +9,18 @@ from calib_tools import rmsce
 import argparse
 
 ANSWER_MAP = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
-LABELS = ["label_1", "label_2", "label_3", "label_4"]
-MODELS = [
-    "model_1",
-    "model_2",
-    "model_3",
-    "model_4",
-]
-COLORS = [0, 1, 4, 3]
+MODELS_TO_LABELS = {
+    "llama-7b": "LLaMA",
+    "rlhf-fixed-llama-v3-bs-16": "RLHF\n(LLaMA)",
+    "shf-v4-llama-10000-kl-0.35": "SuperHF\n(LLaMA)",
+    "llama-instruct-12379": "Instruct",
+    "rlhf-fixed-llama-instruct-bs-16": "RLHF\n(Instruct)",
+    "shf-v4-llama-instruct-10k-kl-0.35": "SuperHF\n(Instruct)",
+}
+MODELS = list(MODELS_TO_LABELS.keys())
+LABELS = list(MODELS_TO_LABELS.values())
+COLORS = [0, 3, 4, 1, 3, 4]
+HATCHES = [""] * 3 + ["//"] * 3
 DO_RESCALE=True
 TEMPERATURE=1.0
 RESCALE_CONST=0.25
@@ -27,7 +31,7 @@ def rescale_softmax_with_temperature(probs, temp, rescale_const):
 
 def compute_calibration_curve(probs, ans):
     diff_bins = np.arange(0, 1.1, 0.1)
-    p_correct_list = np.array([]) 
+    p_correct_list = np.array([])
     for i in range(len(diff_bins) - 1):
         lower = diff_bins[i]
         upper = diff_bins[i + 1]
@@ -51,29 +55,30 @@ if __name__ == "__main__":
     k = args.n_shots
 
     base_dir = "results"
-    base_models = [f.split("results_")[1] for f in MODELS if "results" in f]
-    models = [os.listdir(f"{base_dir}/results_{base_model}") for base_model in base_models]
-    full_model_names = [f"{base_model}/{sub_model}" for base_model, sub_models in zip(base_models, models) for sub_model in sub_models]
+    # base_models = [f.split("results_")[1] for f in MODELS if "results" in f]
+    # models = [os.listdir(f"{base_dir}/results_{base_model}") for base_model in base_models]
+    # full_model_names = [f"{base_model}/{sub_model}" for base_model, sub_models in zip(base_models, models) for sub_model in sub_models]
+    full_model_names = MODELS
     print(full_model_names)
 
     num_plots = len(full_model_names)
 
-    rows = 1
-    cols = num_plots
+    rows = 2
+    cols = 3
     positions = range(1, num_plots + 1)
 
     sns.set_theme(context="paper", font_scale=1.5, style="whitegrid")
     palette = sns.color_palette('colorblind')
     
-    fig = plt.figure(1, figsize=(15,5))
-    fig.suptitle('MMLU Calibration')
-    plt.subplots_adjust(wspace=0.25, left=0.05, right=.95, top=1, bottom=0)
+    fig = plt.figure(1, figsize=(15,10))
+    fig.suptitle('MMLU Calibration Curves')
+    fig.subplots_adjust(wspace=0.25, hspace=-0.1, left=0.05, right=.95, top=1, bottom=0)
 
 
     mses = []
 
     for k, model in enumerate(full_model_names):
-        dir = os.path.join(base_dir, "results_{}".format(model))
+        dir = os.path.join(base_dir, "results_", model)
         fnames = [f for f in os.listdir(dir) if ".csv" in f]
 
         all_max_probs = []
@@ -93,14 +98,14 @@ if __name__ == "__main__":
             max_probs = []
             cors = []
             for i in range(df.shape[0]):
-                probs = [df["{}_choice{}_probs".format(model, choice)][i] for choice in ["A", "B", "C", "D"]]
+                probs = [df[f"/{model}_choice{choice}_probs"][i] for choice in ["A", "B", "C", "D"]]
 
                 all_ans.append(ANSWER_MAP[df[correct_answer_column][i]])
                 if DO_RESCALE:
                     prob = rescale_softmax_with_temperature(probs, args.temp, RESCALE_CONST)
                 all_probs.append(probs)
                 
-                cors.append(int(df["{}_correct".format(model)][i]))
+                cors.append(int(df[f"/{model}_correct"][i]))
                 max_probs.append(np.max(probs))
             all_max_probs += max_probs
             all_cors += cors
@@ -124,7 +129,7 @@ if __name__ == "__main__":
         print(mse)
 
         ax = fig.add_subplot(rows, cols, positions[k])
-        ax.bar(bins, accuracies, width=0.1, edgecolor="black", label=f"{LABELS[k]}", color=palette[COLORS[k]])
+        ax.bar(bins, accuracies, width=0.1, edgecolor="black", label=f"{LABELS[k]}", color=palette[COLORS[k]], hatch=HATCHES[k])
         ax.text(0.25, 0.65, f'MSE:{mse:.3f}', fontsize=14, fontweight='semibold', color='black', horizontalalignment='center')
 
 
@@ -146,8 +151,11 @@ if __name__ == "__main__":
     plt.rcParams.update(plt.rcParamsDefault)
     sns.set_theme(context="paper", font_scale=1.5, style="whitegrid")
     plt.figure()
-    plt.bar(LABELS, mses, color=palette[:len(LABELS)])
-    plt.title("MSE of calibration against $y=x$")
+    colors = [palette[COLORS[i]] for i in range(len(LABELS))]
+    plt.bar(LABELS, mses, color=colors, hatch=HATCHES)
+    plt.title("MSE of Calibration Against $y=x$")
     plt.xlabel("Model")
-    plt.ylabel("MSE")
-    plt.savefig(f"{base_dir}/mse_chart")
+    plt.ylabel("Calibration MSE")
+    plt.gcf().set_size_inches(8, 5)
+    plt.tight_layout()
+    plt.savefig(f"{base_dir}/mse_chart", dpi=300)
